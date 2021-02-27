36f949e7f66d0600b5589d6d94156d637731bb9c
Pulikallu <spulikallu@gmail.com>
2021-02-27 07:36:14 +0530

    intermidate

--- style.js
+++ style.js
@@ -1,11 +1,11 @@
 window.onload = function(){
-  document.querySelector('.viewport-value').innerHTML = "viewport: " + document.documentElement.clientWidth + " x "+ document.documentElement.clientHeight;
+  document.querySelector('.viewport-value').innerHTML = "page viewport: " + document.documentElement.clientWidth + " x "+ document.documentElement.clientHeight;
   document.querySelector('.header-value').innerHTML = "viewport: " +  document.querySelector('.header-value').clientWidth + " x "+  document.querySelector('.header-value').clientHeight;
   document.querySelector('.footer-value').innerHTML = "viewport: " +  document.querySelector('.footer-value').clientWidth + " x "+  document.querySelector('.footer-value').clientHeight;
  }
   
  window.onresize = function(){
-  document.querySelector('.viewport-value').innerHTML = "header: " + document.documentElement.clientWidth + " x "+ document.documentElement.clientHeight;
+  document.querySelector('.viewport-value').innerHTML = "page viewport: " + document.documentElement.clientWidth + " x "+ document.documentElement.clientHeight;
   document.querySelector('.header-value').innerHTML = "header: " +  document.querySelector('.header-value').clientWidth + " x "+  document.querySelector('.header-value').clientHeight;
   document.querySelector('.footer-value').innerHTML = "viewport: " +  document.querySelector('.footer-value').clientWidth + " x "+  document.querySelector('.footer-value').clientHeight;
  }
